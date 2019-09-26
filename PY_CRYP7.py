8171f42699b2d621906435d497c402fe22cfa45c
D4RK-C0D3 <dipannama@gmail.com>
2019-09-26 10:04:31 +0530

    first commit

---
+++ PY_CRYP7.py
@@ -0,0 +1,145 @@
+#HASH codes are started from here
+HASH_CODE = {'a' : 'dhb75rhve358hc34579975gget00u',
+			 'b' : 'eugse578jvww368bse56799kbrd34',
+			 'c' : '6ibde43279jnt544dhi99ygh88744',
+			 'd' : '88ugfr533wsui99jnt667yh457899',
+			 'e' : '799kmgfar67743dung66reyj88788',
+			 'f' : '88iuhhhi778844tgar77890oppkaw',
+			 'g' : '1479uf357jbaxzb,juzryj853dhio',
+			 'h' : '7888jfarummcxznmgfdkltd7834sy',
+			 'i' : 'ujfyhedywurui5fuf7ugfc5yq8itk',
+			 'j' : '738jjw8je0jwhw601653810jheyyh',
+			 'k' : '78j3hrh7infncmbjsyjw8929i3je8',
+			 'l' : '7u37hebttu2892be67h4wi892jwn7',
+			 'm' : 'yh378be67i01b37uhbe802ih3jii2',
+			 'n' : '0uy3henyh2i8i2h2h2h9in4beauhb',
+			 'o' : 'iijeuh299u4ybei8ij2hhwnbsuei9',
+			 'p' : '904838jnebsydgw729h4ebjsjse79',
+			 'q' : '78383i393bs8wuueheneie8i2jwke',
+			 'r' : '7iiiejikwjhsujeuejehuwjen99j4',
+			 's' : '52902kwmbsba5yhw882nsg7hhejjw',
+			 't' : '67383hbesyund8iwj3nw9iwjwneej',
+			 'u' : '62882hebdu62jnsmuieejhdeji3u8',
+			 'v' : '537u2bwbsys6bsbmiw6gehe773jej',
+			 'w' : '638jebdu7yejrn9whwby73hrhhue7',
+			 'x' : '53793jndn77y7euueuejn3u8rjrni',
+			 'y' : '63784u47u4h4r8rwihfbhwue7i4rj',
+			 'z' : '53883jenhy273uhrhu7uejrjuuuer',
+			 'A' : '58393ubsbhathsnhtha38iehdbhe7',
+			 'B' : '538i3nsbayyue573jnwhayueur738',
+			 'C' : '4738jebshauuwhjs8ygey7wwuue63',
+			 'D' : '47282jsbhsuhrh7e727284jng8498',
+			 'E' : '683hehgu2geh8ujsnkdmmduudjeij',
+			 'F' : '425twgeh8e8hdbjmjsuowkqkllhah',
+			 'G' : '57ehebuuhebowuwgwbeb937638wbe',
+			 'H' : '5525272h3vsbihsvdtuwijwbdbehu',
+			 'I' : '562653773hdbuejdyy2hebdbiejhe',
+			 'J' : '562737365738hbshu2yywuehduu3e',
+			 'K' : '45283uhehbsbshiehdhyeudjjeiei',
+			 'L' : '6ywhehy2hvsvsu7e7w82wjbue7ehw',
+			 'M' : 'uhhhwi8wjwjwjwiwiwiwwi8837739',
+			 'N' : '6747bew992u3uehrhjeuywuiwwiw8',
+			 'O' : '31782i3hghei010u3hehde7uehjwj',
+			 'P' : '46282jwhbschsjxueuh7swi9892iw',
+			 'Q' : '938uhh6eyvsjwiyehi7eu26e892je',
+			 'R' : '52783jbyuheh73ueh752992jnenui',
+			 'S' : '56383jnsbhwu628ndnhy788euuuhj',
+			 'T' : 'hujjdjiejehuehjeiiiejeuiejri3',
+			 'U' : 'gyuwyw5728whb6y3782jdnuudw7he',
+			 'V' : '66782hwhysgwjs85893jendieei93',
+			 'W' : '56282jwne77whjjwjwjjwjwjjwejj',
+			 'X' : 'hwhhsyywyhuwuuhwuhwheuheju8eu',
+			 'Y' : 'hhwjjwjwujwnjieijejehhjnsnsjs',
+			 'Z' : 'gyw56wuhwhwhwhjwi893ijjeujeuw',
+			 '0' : '58u2g257282bsvhsgshdjueusueje',
+			 '1' : '42883hdu783ndnxbj7e6uejhhrjdu',
+			 '2' : 'yeu77383jndhe77d63hbu7eihhe77',
+			 '3' : 'ggsgswy582gd7ehs636u2he78838u',
+			 '4' : '562geye7ue78ejnyud773idh3ii3i',
+			 '5' : 'ygs562bdge76jehduy3ej7ejheu73',
+			 '6' : '5638ehgyiy6jdyheeyj4tigvjd66x',
+			 '7' : 'ueuurc7tkfu6dywruuvurehfuufiu',
+			 '8' : '7uyedbu4suo8776hbhhjjjgryy4wy',
+			 '9' : '478jbesh78bu6ggfdruf5fgu88igf',
+			 '!' : 'yehs62gw59eh68ebye7ejhe7eu3e7',
+			 '`' : '683hdb7i2973hdh8hehjw8yejio09',
+			 '@' : '429wjsnghwti38u2ye73jenhiee89',
+			 '~' : '5738j3bsby7282jegu82kejyu2t72',
+			 '$' : '5829hwbesbjiwuwjyy3iui37y9weu',
+			 '_' : '57w8hshhwt3ihs682jbsue72ut8wi',
+			 '%' : '627iwhg7sj3hxgyejwt28jsngjw78',
+			 '=' : '6383hbe893iehheijeheueje7heje',
+			 '^' : 'yi3u3uejueu3ejuej3i279kenedee',
+			 '+' : '10192iheii3je7jendmxjieo39029',
+			 '&' : '6738ehdbyhsus9w0j3nsbshie7i2u',
+			 '{' : '6839ejdnmuuehydei62939hdjdii3',
+			 '*' : '53882jshus7jebtu3je67hsheue83',
+			 '}' : '5738whhskw86shjs863i28iehdeu3',
+			 '(' : '682uehbs773jehu73ijdh7wiejd38',
+			 '[' : '729jwnsnzhue8eyhmsuyu2j2eui3i',
+			 ')' : 'tu2638j3hsbu73hhe773heh77iejj',
+			 ']' : 'tyw672jsbsy733uh7u3hwyu3e7398',
+			 '|' : '728jwnsnsuw2uhyihw89jekmd9dbv',
+			 '<' : 'mznjzghs9w92673hdu7wij2j21iwe',
+			 ';' : '68292henyyejb3i2oji20jsnhxuei',
+			 ',' : '67383hey5uebbdjyeuey8kendjei8',
+			 '>' : '57wuhshnbdge7682hehyiejhweu37',
+			 ':' : '5527eubskyssye7e68wjsndhueu3i',
+			 '.' : '67eindy73hd73i7376j4njdeu738u',
+			 '/' : '5638hehdu7ye3iejeu83694jnhd7e',
+			 '?' : '6638hhd77eiuj7s73jh3yieuhe3hu',
+			 '#' : '738832929hebsduejd6393jnde738',
+			 '\\' : '7839jsh68udb83i9kemxue84jne8',
+			 '"' : '5389jebs6948jrhd884jr8deje8je',
+			 '-' : '5682jbw002jbagagw9002iwnnwsuh',
+}
+
+#defining the encryption process of the given text
+def encryptor(text):
+	encrypted_text = ""
+	for letters in text:
+		if letters != " ":
+			encrypted_text = encrypted_text + HASH_CODE.get(letters) + " "
+		else:
+			encrypted_text += " "
+	print("\n\nYour encrypted text is : " + encrypted_text)
+		
+#defining the decryption process of the given text
+def decryptor(text):
+	text += " "
+	key_list = list(HASH_CODE.keys())
+	value_list = list(HASH_CODE.values())
+	hash = ""
+	decrypted_text = ""
+	for letters in text:
+		if letters != " ":
+			hash += letters
+			space = 0
+		else:
+			space += 1
+			if space == 2:
+				decrypted_text += " "
+			else:
+				decrypted_text = decrypted_text + key_list[value_list.index(hash)]
+				hash = ""
+	print("\n\nYour decrypted text is : " + decrypted_text)
+	
+#taking the values from the user
+print("\n\n\n\t\t[HASH CODE GENERATOR]")
+user_input = input("\n\nPress 'E' to Encrypt Or 'D' to Decrypt : ")
+
+#taking the values from the user to encrypt 
+if user_input == 'E' or user_input == 'e':
+	text_to_encrypt = input("Enter your text to encrypt : ")
+	encryptor(text_to_encrypt)
+
+#taking the values from the user to decrypt 
+elif user_input == 'D' or user_input == 'd':
+	text_to_decrypt = input("Enter your hash code to decrypt : ")
+	decryptor(text_to_decrypt)
+
+#Error exception handeling
+else:
+	print("Error! Please enter the correct information!")
+
+#this code is copyrighted by D4RK-C0D3
